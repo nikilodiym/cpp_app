282e2c8b8a7197be15852e92dae10d2793d049f8
Nikita Krenov <nikitabaram@gmail.com>
2024-11-02 13:57:26 +0200

    Add Book struct and map to manage library collection
    Included <map> header to use map container. Defined a new struct named Book to store book information. Created a map named library in the main function to store Book objects with ISBN as the key. Added three Book objects to the library map. Implemented retrieval and printing of a book's details by ISBN. Removed system("pause"); line.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <string>
+#include <map>
 
 using namespace std;
 
+struct Book {
+    string title;
+    string authors;
+    int year;
+};
+
 int main() {
+    map<string, Book> library;
+
+    Book book1 = { "The Great Gatsby", "F. Scott Fitzgerald", 1925 };
+    library["9780743273565"] = book1;
+
+    Book book2 = { "To Kill a Mockingbird", "Harper Lee", 1960 };
+    library["9780061120084"] = book2;
+
+    Book book3 = { "1984", "George Orwell", 1949 };
+    library["9780451524935"] = book3;
+
+    string isbn = "9780451524935";
+	cout << "----------------------------------------" << endl;
+    cout << "---Book with ISBN " << isbn << ":" << endl;
+    cout << "---Title: " << library[isbn].title << endl;
+    cout << "---Authors: " << library[isbn].authors << endl;
+    cout << "---Year of Publication: " << library[isbn].year << endl;
+	cout << "----------------------------------------" << endl;
 
 	system("pause");
-	return 0;
+    return 0;
 }
